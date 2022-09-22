650a7cd4915d860dfc0b07a592bf164a6fb2e0a6
Lyubomir <lyubomir@5atech.io>
2022-09-22 08:46:20 +0300

    Add a simple .csv file and update readme.md

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # kiwi-tcms-import-util
 utility to import test cases in Kiwi TCMS using a CSV
+
+In order to simply import test cases into an existing Test plan one needs to fill the Test cases.csv
+with the desired test cases. Then edit main.py by entering your Test plan id.
+```python
+            self.rpc_client.exec.TestPlan.add_case(
+                7, test_case["id"]
+            )  # 7 is the ID of the TestPlan where the testcase will be added
+```
+
+Run the program and the tests will be updated in the test plan.
+
+
+If you face any SSL issues check [KIWI dock](https://kiwitcms.readthedocs.io/en/latest/installing_docker.html?highlight=ssl#enable-plain-text-http-access:~:text=environment%3A%0A%20%20%20%20KIWI_DONT_ENFORCE_HTTPS%3A%20%22true%22
+)
--- main.py
+++ main.py
@@ -5,10 +5,10 @@ import datetime
 from ReadCsv import ReadCsv
 
 
-class Main():
+class Main:
 
     ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
-    CSV_PATH = glob.glob(os.path.join(ROOT_DIR, 'csv/')+"*.csv")
+    CSV_PATH = glob.glob(ROOT_DIR + "/*.csv")
     rpc_client = TCMS()
 
     def csvToList(self):
@@ -35,64 +35,65 @@ class Main():
                 finalList[j].append(csv[k])
                 k = k + 1
             j = j + 1
-            if  j == i or j > i:
+            if j == i or j > i:
                 break
 
         print(finalList)
 
-
-
     def createTestPlan(self):
-        test_plan = self.rpc_client.exec.TestPlan.create({
-            'name': 'Foree Mobile App From Script',
-            'text': 'Function Testing, Integration Testing, Regression Testing',
-            'type': 11,  # Performance
-            'product': 2,
-            'product_version': 4,
-            'is_active': True,
-        })
+        test_plan = self.rpc_client.exec.TestPlan.create(
+            {
+                "name": "Foree Mobile App From Script",
+                "text": "Function Testing, Integration Testing, Regression Testing",
+                "type": 11,  # Performance
+                "product": 2,
+                "product_version": 4,
+                "is_active": True,
+            }
+        )
 
-    def formatString(self,listOfString):
-        formattedString = "";
+    def formatString(self, listOfString):
+        formattedString = ""
         for s in listOfString:
-            formattedString = formattedString + s + '\n'
+            formattedString = formattedString + s + "\n"
         return formattedString
-
-
-
 
     def createTestCases(self):
         test_cases = self.csvToList()
-        for idx,case in enumerate(test_cases):
+        for idx, case in enumerate(test_cases):
             val = case[0]
-            break_list = val.split(',')
-            list_of_desc = break_list[1].split('\\n')
-            desc = self.formatString(list_of_desc)
-            list_of_steps = break_list[3].split('\\n')
+            break_list = val.split(",")
+            list_of_desc = break_list[0]
+            desc = list_of_desc
+            list_of_steps = break_list[1].split("\\n")
             steps = self.formatString(list_of_steps)
-            list_of_data = break_list[4].split('\\n')
+            list_of_data = break_list[2].split("\\n")
             data = self.formatString(list_of_data)
-            list_of_expected = break_list[6].split('\\n')
+            list_of_expected = break_list[3].split("\\n")
             expected_results = self.formatString(list_of_expected)
-            is_automated = break_list[5]
-            text = f'**Data: ** \n {data} \n\n\n**Steps: ** \n {steps} \n\n\n **Expected :** \n {expected_results}'
-            test_case = self.rpc_client.exec.TestCase.create({
-                'summary': desc,
-                'text': text,
-                'product': 2,
-                'category': 4,
-                'priority': 1,
-                'case_status': 2,  # CONFIRMED
-                'author': 'on',
-                'is_automated': is_automated,
-                'managers_of_runs': 'on',
-                'assignees_of_case_runs': 'on',
-                'default_tester_of_case': 'on',
-                'default_testers_of_runs': 'on',
-                'notify_on_case_update': 'on',
-                'notify_on_case_delete': 'on'
-            })
-            self.rpc_client.exec.TestPlan.add_case(4, test_case['case_id'])
+            is_automated = break_list[4].strip().lower() in ["true"]
+            text = f"**Test case data: ** \n {data} \n\n\n**Steps: ** \n {steps} \n\n\n **Expected :** \n {expected_results}"
+            test_case = self.rpc_client.exec.TestCase.create(
+                {
+                    "summary": desc,
+                    "text": text,
+                    "product": 2,  # the ID of the product.
+                    "category": 4,
+                    "priority": 1,
+                    "case_status": 2,  # CONFIRMED
+                    "author": "2",
+                    "is_automated": is_automated,
+                    "managers_of_runs": "on",
+                    "assignees_of_case_runs": "on",
+                    "default_tester_of_case": "on",
+                    "default_testers_of_runs": "on",
+                    "notify_on_case_update": "on",
+                    "notify_on_case_delete": "on",
+                }
+            )
+            self.rpc_client.exec.TestPlan.add_case(
+                7, test_case["id"]
+            )  # 7 is the ID of the TestPlan where the testcase will be added
 
 
 m = Main()
